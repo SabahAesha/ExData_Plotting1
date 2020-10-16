0b37c6664f7a3de2a2dc1e911e734f0e67ad1ffc
SabahAesha <71190801+SabahAesha@users.noreply.github.com>
2020-10-16 17:33:56 +0600

    Add files via upload

---
+++ Plot1.R
@@ -0,0 +1,9 @@
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+data <- subset(data,Date>=as.Date("2007-2-1")&Date<=as.Date("2007-2-2"))
+data <- data[complete.cases(data),]
+dateTime <- paste(data$Date, data$Time)
+dateTime <- setNames(dateTime, "DateTime")
+data <- data[ ,!(names(data) %in% c("Date","Time"))]
+data <- cbind(dateTime, data)
+hist(data$Global_active_power, main="Global Active Power", xlab = "Global Active Power (kilowatts)", col="red")
